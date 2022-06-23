dcedce5019f43a5d4d63a29beda1f8ee81232460
mouhib <iwritebadcode@github.com>
2022-06-23 02:01:15 +0100

    configured ts

---
+++ types.ts
@@ -0,0 +1,8 @@
+//this file contains all type definitions for the whole app
+
+export interface IHomeScreenData {
+	id: number;
+	title: string;
+	description: string;
+	image: any;
+}
---
+++ welcomeScreen.ts
@@ -0,0 +1,12 @@
+import { IHomeScreenData } from './types';
+
+let welcomeData: IHomeScreenData[];
+
+export default welcomeData = [
+	{
+		id: 1,
+		title: 'Full data storage',
+		description: 'keep track of your score on a regular basis',
+		image: '',
+	},
+];
