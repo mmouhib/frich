cda2e964c54d55b850e37589f815727093b4c83d
mouhib <iwritebadcode@github.com>
2022-07-03 06:02:26 +0100

    added `useDimentions` hook to fix screen height value

--- App.tsx
+++ App.tsx
@@ -3,21 +3,28 @@ import { StyleSheet, View, useWindowDimensions } from 'react-native';
 import Welcome from './src/views/Welcome';
 import { ScaledSize } from 'react-native';
 import Signup from './src/views/SignUp';
+import colors from './src/utils/colors';
+import useDimentions from './src/hooks/useDimensions';
 
 export default function App() {
-	const { height, width }: ScaledSize = useWindowDimensions();
+	// const { height, width }: ScaledSize = useWindowDimensions();
+
+	//import useDimentions from './src/hooks/useDimensions';
+	const width = useDimentions().exactWidth;
+	const height = useDimentions().exactHeight;
 
 	return (
 		<View style={[styles.container, { width: width, height: height }]}>
 			<StatusBar style="auto" />
-			{/* <Welcome /> */}
-			<Signup />
+			<Welcome />
+			{/* <Signup /> */}
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
+		backgroundColor: colors.lightGlass,
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
