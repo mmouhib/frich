113786bdf1a04d75187d13dffe70dbb6684a16a9
mouhib <iwritebadcode@github.com>
2022-07-04 19:31:38 +0100

    switched to `SplashScreen` for pre-launch imports

--- App.tsx
+++ App.tsx
@@ -1,27 +1,47 @@
+import { useState, useEffect, useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Text } from 'react-native';
 import Welcome from './src/views/Welcome';
 import Signup from './src/views/SignUp';
 import colors from './src/utils/colors';
 import useDimentions from './src/hooks/useDimensions';
-
-import AppLoading from 'expo-app-loading';
-
-import { useFonts, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import { Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
 	const { exactHeight, exactWidth } = useDimentions();
 
-	let [fontsLoaded] = useFonts({
-		Roboto_700Bold,
-	});
+	const [isReady, setIsReady] = useState<boolean>(false);
 
-	if (!fontsLoaded) {
-		return <AppLoading />;
+	useEffect(() => {
+		(async () => {
+			try {
+				await SplashScreen.preventAutoHideAsync();
+				await Font.loadAsync({ Roboto_700Bold });
+			} catch (e) {
+				console.log(e);
+			} finally {
+				setIsReady(true);
+			}
+		})();
+	}, []);
+
+	const _onLayout = useCallback(() => {
+		if (isReady) {
+			SplashScreen.hideAsync();
+		}
+	}, [isReady]);
+
+	if (!isReady) {
+		return null;
 	}
 
 	return (
-		<View style={[styles.container, { width: exactWidth, height: exactHeight }]}>
+		<View
+			style={[styles.container, { width: exactWidth, height: exactHeight }]}
+			onLayout={_onLayout}
+		>
 			<StatusBar style="auto" />
 			{/* <Welcome /> */}
 			<Signup />
