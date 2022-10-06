c3d42ff2c5c240b0a975ac5f0ea0329b7f4af6d9
Mouhib Ouni <mouhibouni321@gmail.com>
2022-10-07 00:15:45 +0100

    Start matches list component.

---
+++ src/components/MatchSection/MatchSecreenBox.tsx
@@ -0,0 +1,14 @@
+import { View, StyleSheet } from 'react-native';
+import { MatchScreenProps } from '../../views/match/MatchesList';
+
+interface MatchScreenBoxProps {
+	data: MatchScreenProps;
+}
+
+export default function MatchScreenBox(data: MatchScreenBoxProps) {
+	return <View></View>;
+}
+
+const styles = StyleSheet.create({
+	container: {},
+});
--- src/views/match/MatchesList.tsx
+++ src/views/match/MatchesList.tsx
@@ -1,7 +1,26 @@
 import { View, StyleSheet } from 'react-native';
+import MatchScreenBox from '../../components/MatchSection/MatchSecreenBox';
+
+export interface MatchScreenProps {
+	matchType: string;
+	matchDate: string;
+	duration: string;
+	won: boolean;
+}
 
 export default function MatchesList() {
-	return <View style={styles.container}></View>;
+	let matchBoxProps = {
+		matchType: 'Zwez',
+		matchDate: '11/3/2022',
+		duration: '24 minutes',
+		won: true,
+	} as MatchScreenProps;
+
+	return (
+		<View style={styles.container}>
+			<MatchScreenBox data={matchBoxProps} />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
